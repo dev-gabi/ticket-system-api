b801b49384a13ffd347df203e17081bec0639251
gabi <my@email.com>
2022-12-11 04:11:33 +0200

    logs entities

---
+++ Entities/code-first/logs/AuthLog.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class AuthLog:Log
+    {
+        [Required, MaxLength(6)]
+        public string Action { get; set; }
+    }
+}
---
+++ Entities/code-first/logs/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public abstract class Log : DBEntity
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string Details { get; set; }
+        [Required]
+        public string UserId { get; set; }
+    }
+}
